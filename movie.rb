b4c5286616ebbc81ec2788dc8dd287930c517608
mahbubul14 <mahbub21alam2017@gmail.com>
2022-01-19 21:42:06 +0600

    Update all methods for movies

--- movie.rb
+++ movie.rb
@@ -1,7 +1,7 @@
 require_relative './item'
 
 class Movie < item
-  attr_reader :
+  attr_reader :silent
 
   # rubocop:disable Metrics/ParameterLists
   def initialize(genre, author, source, label, publish_date, silent)
@@ -12,5 +12,21 @@ class Movie < item
 
   def can_be_archived?
     super or silent
+  end
+
+  def to_s
+    "[Movie]: #{super}"
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => id,
+      'genre' => @genre.id,
+      'author' => @author.id,
+      'source' => @source.id,
+      'label' => @label.id,
+      'silent' => @silent
+    }.to_json(*args)
   end
 end
