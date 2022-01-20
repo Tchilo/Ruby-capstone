751c710d1ce4935f1717366e6d99ca9f1845a9a8
yahya <crazy20046@gmail.com>
2022-01-20 14:49:11 +0200

    Add the author and game table

--- schema.sql
+++ schema.sql
@@ -3,10 +3,12 @@ CREATE DATABASE 'catalog';
 CREATE TABLE item(
   id INT GENERATED ALWAYS AS IDENTITY,
   source_id INT,
+  author_id INT,
   publish_date DATE,
   archived BOOLEAN,
   PRIMARY KEY(id),
   FOREIGN KEY (source_id) REFERENCES sources (id),
+  FOREIGN KEY (author_id) REFERENCES authors (id)
 );
 
 CREATE TABLE sources(
@@ -20,5 +22,20 @@ CREATE TABLE movies(
   silet BOOLEAN,
   FOREIGN KEY(id) REFERENCES item(id)
 );
+
+CREATE TABLE author(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE games(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  multiplayer BOOLEAN,
+  last_played_at DATE,
+  FOREIGN KEY(id) REFERENCES item(id)
+);
+
 
 
